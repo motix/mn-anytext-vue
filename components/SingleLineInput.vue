9a068a8747e5f6c91c2f16e4200c9a3b9e5fd1b6
minh.nguyen@motiteam.com <minh.nguyen@motiteam.com>
2019-02-08 01:00:29 +0700

    0.0.2

--- components/SingleLineInput.vue
+++ components/SingleLineInput.vue
@@ -6,27 +6,94 @@
     import { SingleLineInput } from 'mn-anytext';
 
     export default {
-        props: ['value', 'placeholder', 'placeholder-css-class'],
+        props: {
+            value: null,
+            placeholder: String,
+            'placeholder-css-class': String,
+            'placeholder-align': String,
+            wrapping: {
+                type: Boolean,
+                default: true
+            },
+            prefix: String,
+            suffix: String
+        },
         data() {
             return {
-                input: null
+                input: null,
+                converter: {
+                    toNumber(args) {
+                        if (args.input === null || args.input === '') {
+                            args.result = '';
+                            return;
+                        }
+                        args.result = Number(args.input);
+                        if (isNaN(args.result)) {
+                            args.result = '';
+                        }
+                    },
+
+                    fromNumber(args) {
+                        if (args.output === null || args.output === '') {
+                            args.result = null;
+                            return;
+                        }
+                        args.result = Number(args.output);
+                        if (isNaN(args.result)) {
+                            args.result = '';
+                        }
+                    },
+                    toPercent(args) {
+                        if (args.input === null || args.input === '') {
+                            args.result = '';
+                            return;
+                        }
+                        args.result = Number((Math.round(args.input * 10000) / 100).toFixed(2));
+                        if (isNaN(args.result)) {
+                            args.result = '';
+                        }
+                    },
+
+                    fromPercent(args) {
+                        if (args.output === null || args.output === '') {
+                            args.result = null;
+                            return;
+                        }
+                        args.result = Number((Math.round(args.output * 100) / 10000).toFixed(4));
+                        if (isNaN(args.result)) {
+                            args.result = null;
+                        }
+                    }
+                }
             };
         },
         watch: {
             value(newValue) {
-                this.input.value = newValue ;
+                const args = { input: newValue, result: newValue };
+                this.$emit('convert-input', this, args);
+                this.input.value = args.result;
             }
         },
         mounted() {
-            var that = this;
+            const that = this;
+
+            const args = { input: this.value, result: this.value };
+            this.$emit('convert-input', this, args);
+
             this.input = new SingleLineInput(this.$el, {
-                value: this.value,
+                value: args.result,
                 placeholder: {
                     emptyInputCssClass: this.placeholderCssClass,
-                    emptyInputText: this.placeholder
+                    emptyInputText: this.placeholder,
+                    align: this.placeholderAlign
                 },
+                wrapping: this.wrapping,
+                prefix: this.prefix,
+                suffix: this.suffix,
                 onChange() {
-                    that.$emit('input', that.input.value);
+                    const args = { output: that.input.value, result: that.input.value };
+                    that.$emit('convert-output', that, args);
+                    that.$emit('input', args.result);
                 }
             });
         }
@@ -34,15 +101,28 @@
 </script>
 
 <style>
-    @supports (-moz-appearance: none) {
-        .ProseMirror[contenteditable] {
-            white-space: pre-wrap;
-        }
+    [data-prefix-text]:first-child::before {
+        content: attr(data-prefix-text);
+        float: left;
     }
 
-    [data-empty-input-text]:first-child::before {
-        content: attr(data-empty-input-text);
-        height: 0;
-        float: left;
+    [data-suffix-text]:first-child::before {
+        content: attr(data-suffix-text);
+        float: right;
+    }
+
+    [data-empty-input-text] {
+        &:first-child::before {
+            content: attr(data-empty-input-text);
+            height: 0;
+        }
+        
+        &[data-empty-input-align=left]:first-child::before {
+            float: left;
+        }
+        
+        &[data-empty-input-align=right]:first-child::before {
+            float: right;
+        }
     }
 </style>
